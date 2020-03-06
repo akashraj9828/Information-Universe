cfa130b572e36c6783725874b8971cc57cb31965
Akash raj(CMD) <akashraj9828@gmail.com>
2020-03-06 14:19:27 +0530

    modularized

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ var bodyParser = require('body-parser')
 const https = require('https');
 const request = require('request')
 
-const default_img="https://i.ibb.co/7jZZX53/no-img.png"
+var dateFormat = require('dateformat');
+process.env.TZ = 'Asia/Kolkata'
+const default_img = "https://i.ibb.co/7jZZX53/no-img.png"
 
-var url="https://akashraj.tech/api/webhook_info_uni.php"
-var webhook_url=get_webhook(url)
-var not_random_list=["Engineering","Astronomy","Biology","Chemistry","Cognitive Science","Computer Science","Ecology","Geography","Geology","Linguistics","Physics","Psychology","Sociology","Political","Zoology","Volcanology","Meteorology","Paleontology","Methodology","Geometry","matter","motion","spacetime","Astrophysics","Astrometry","Optics","Neurophysics","laws of thermodynamics","Computer","Central processing unit","Computer memory","Processor design","History of computing hardware","Integrated circuit","Logic gate","Microprocessor","Microcontroller","Very Large Scale Integration","Intel 4004","Travelling salesman problem", "Minimum spanning tree", "Shortest path problem", "Dijkstra's algorithm", "Greedy algorithm", "A* search algorithm", "Bellman–Ford algorithm", "Engine", "Reciprocating engine", "Timeline of motor and engine technology", "Petrol engine", "Four-stroke engine", "Wood gas", "Stirling engine", "Brayton cycle", "Gas engine", "Hydrolock", "Automotive engine","Diesel engine", "Reciprocating engine", "Miller cycle", "Two-stroke engine", "Stratified charge engine", "Four-stroke engine","India", "Hindus", "West Bengal", "Kolkata", "Partition of India", "South India", "Islam in India", "Economic history of India", "Indian Councils Act 1909", "Independence Day (India)", "Partition of Bengal (1905)","Akbar", "Humayun", "Jahangir", "Second Battle of Panipat", "Mughal emperors", "Bairam Khan", "Abdul Rahim Khan-I-Khana", "Maldev Rathore", "Bharmal", "Khusrau Mirza", "Hamida Banu Begum", "Daniyal Mirza", "Murad Mirza (son of Akbar)", "Ruqaiya Sultan Begum", "Salima Sultan Begum","Taj Mahal", "Shah Jahan", "Agra", "Mumtaz Mahal", "Agra Fort", "Jama Masjid, Delhi", "Mughal architecture", "Tomb of I'timād-ud-Daulah", "Indo-Islamic architecture", "Bibi Ka Maqbara", "Tomb of Jahangir", "Tomb of Safdar Jang", "Akbar's tomb", "GitHub", "Nat Friedman", "Git", "Comparison of source-code-hosting facilities", "Open Hub", "Magento", "StyleCop", "Etherpad", "Bitbucket", "Andreessen Horowitz", "Twilio", "Meteor (web framework)", "Tom Preston-Werner", "Jekyll (software)", "GitLab","Google", "General Electric", "Microsoft", "Larry Page", "Yahoo!", "Eric Schmidt", "Gmail", "Google Maps", "Baidu", "YouTube"]
+// create a WEBHOOK_URL enviorment variable.
+// plug the url there
+var webhook_url = check_key(process.env, "WEBHOOK_URL") ? process.env.WEBHOOK_URL : undefined
+console.log("webhook_url: ", webhook_url)
+
+
+
+var not_random_list = ["Brooklyn Nine-Nine", "Last Man Standing(American TV series)", "Dads(2013 TV series)", "The Blacklist(TV series)", "Enlisted(TV series)", "Surviving Jack", "List of Brooklyn Nine-Nineepisodes", "Mulaney", "Superstore(TV series)", "New Girl(season 6)", "Pilot (Brooklyn Nine-Nine)", "Christmas (Brooklyn Nine-Nine)", "The Slump", "Pontiac Bandit", "Undercover (Brooklyn Nine-Nine)", "The Mole (Brooklyn Nine-Nine)", "Johnny and Dora", "The Oolong Slayer", "The Box (Brooklyn Nine-Nine)", "Jake & Amy", "Brooklyn Nine-Nine(season 6)", "Engineering", "Astronomy", "Biology", "Chemistry", "Cognitive Science", "Computer Science", "Ecology", "Geography", "Geology", "Linguistics", "Physics", "Psychology", "Sociology", "Political", "Zoology", "Volcanology", "Meteorology", "Paleontology", "Methodology", "Geometry", "matter", "motion", "spacetime", "Astrophysics", "Astrometry", "Optics", "Neurophysics", "laws of thermodynamics", "Computer", "Central processing unit", "Computer memory", "Processor design", "History of computing hardware", "Integrated circuit", "Logic gate", "Microprocessor", "Microcontroller", "Very Large Scale Integration", "Intel 4004", "Travelling salesman problem", "Minimum spanning tree", "Shortest path problem", "Dijkstra's algorithm", "Greedy algorithm", "A* search algorithm", "Bellman–Ford algorithm", "Engine", "Reciprocating engine", "Timeline of motor and engine technology", "Petrol engine", "Four-stroke engine", "Wood gas", "Stirling engine", "Brayton cycle", "Gas engine", "Hydrolock", "Automotive engine", "Diesel engine", "Reciprocating engine", "Miller cycle", "Two-stroke engine", "Stratified charge engine", "Four-stroke engine", "India", "Hindus", "West Bengal", "Kolkata", "Partition of India", "South India", "Islam in India", "Economic history of India", "Indian Councils Act 1909", "Independence Day (India)", "Partition of Bengal (1905)", "Akbar", "Humayun", "Jahangir", "Second Battle of Panipat", "Mughal emperors", "Bairam Khan", "Abdul Rahim Khan-I-Khana", "Maldev Rathore", "Bharmal", "Khusrau Mirza", "Hamida Banu Begum", "Daniyal Mirza", "Murad Mirza (son of Akbar)", "Ruqaiya Sultan Begum", "Salima Sultan Begum", "Taj Mahal", "Shah Jahan", "Agra", "Mumtaz Mahal", "Agra Fort", "Jama Masjid, Delhi", "Mughal architecture", "Tomb of I'timād-ud-Daulah", "Indo-Islamic architecture", "Bibi Ka Maqbara", "Tomb of Jahangir", "Tomb of Safdar Jang", "Akbar's tomb", "GitHub", "Nat Friedman", "Git", "Comparison of source-code-hosting facilities", "Open Hub", "Magento", "StyleCop", "Etherpad", "Bitbucket", "Andreessen Horowitz", "Twilio", "Meteor (web framework)", "Tom Preston-Werner", "Jekyll (software)", "GitLab", "Google", "General Electric", "Microsoft", "Larry Page", "Yahoo!", "Eric Schmidt", "Gmail", "Google Maps", "Baidu", "YouTube", "Goldman Sachs", "Investment banking", "Lehman Brothers", "Bear Stearns", "Collateralized debt obligation", "Henry Paulson", "Lloyd Blankfein", "Merrill Lynch & Co.", "IKB Deutsche Industriebank", "Asset-backed securities index", "Subprime crisis impact timeline", "Synthetic CDO", "Spider-Man", "Spider-Man", "The Amazing Spider-Man", "Steve Ditko", "Amazing Fantasy", "Tales to Astonish", "John Romita Sr.", "John Romita Jr.", "Goku", "Dragon Ball (manga)", "Gohan", "Frieza", "Vegeta", "Piccolo (Dragon Ball)", "Krillin", "Neko Majin", "Trunks (Dragon Ball)", "List of Dragon Ball characters", "Marvel", "DC Comics", "Marvel Comics", "Alpha (disambiguation)", "WildStorm", ]
 var random_list = []
 var random_generated = false
 var random_count = 0
@@ -21,7 +28,8 @@ if (!random_generated) {
     random_generated = true
 }
 
-var coin=true
+var coin = true
+
 function listen() {
     var host = server.address().address;
     var port = server.address().port;
@@ -54,9 +62,9 @@ app.post('/get-data', function (req, res) {
         })
     } else if (req.body.random == 1) {
 
-        
-        random_topic = coin ? not_random_list[between(0,not_random_list.length)]:random_list[between(0, random_list.length)]
-        coin!=coin
+
+        random_topic = coin ? not_random_list[between(0, not_random_list.length)] : random_list[between(0, random_list.length)]
+        coin != coin
         query_wiki(random_topic, function (a) {
             result = a
             build_msg(result)
@@ -98,6 +106,7 @@ function query_wiki(title, callback) {
         res.title = data.title
         res.description = data.description
         res.extract = data.extract
+        res.link = check_key(data, "content_urls") ? data.content_urls.desktop.page : "https://wikipedia.com"
         res.image = check_key(data, "thumbnail") ? data.thumbnail.source : default_img
         related[data.displaytitle] = pack(data, -1)
 
@@ -127,8 +136,8 @@ function pack(data, count) {
     temp_obj.title = check_key(elm, "displaytitle") ? elm.displaytitle : undefined
     temp_obj.description = check_key(elm, "description") ? elm.description : undefined
     temp_obj.extract = check_key(elm, "extract") ? elm.extract : undefined
-    temp_obj.link = check_key(elm, "content_urls") ? elm.content_urls.desktop.page : default_img
-    temp_obj.image = check_key(elm, "thumbnail") ? elm.thumbnail.source : undefined
+    temp_obj.link = check_key(elm, "content_urls") ? elm.content_urls.desktop.page : "https://wikipedia.com"
+    temp_obj.image = check_key(elm, "thumbnail") ? elm.thumbnail.source : default_img
     temp_obj.count = count
 
     return temp_obj
@@ -174,72 +183,104 @@ function make_random_list() {
 
     }).on("error", (error) => {
         console.log("Error getting random: " + error.message);
-        make_random_list()
+        console.log("Retrying in 10s");
+        setTimeout(() => {
+            make_random_list()
+        }, 10000);
 
     });
     return
 
 }
-msg = {
+var msg = {
     "blocks": []
 }
+var msg_count = 0
+var msg_lenght = 5
+var warned = false
 
 function build_msg(data) {
-    if (msg == {}) {
-        msg["blocks"] = []
+    if (webhook_url) {
+        msg_count += 1
+        if (msg["blocks"].length == 0) {
+            msg["blocks"].push({
+                "type": "context",
+                "elements": [{
+                    "type": "mrkdwn",
+                    "text": `*Date and time:* ${dateFormat("dddd, mmmm dS, yyyy, h:MM TT")}`
+                }]
+            })
+
+            msg["blocks"].push({
+                    "type": "section",
+                    "fields": []
+                }
+
+            )
+            // console.log(msg['blocks']);
+
+        }
+
+
+        msg["blocks"][1]["fields"].push(list_item(data.title, data.description, data.link))
+        // msg["blocks"].push(block_item(data.title, data.link, data.description, data.extract, data.image))
+        // msg["blocks"].push({
+        // "type": "divider"
+        // })
+
+
+
+        if (msg_count >= msg_lenght) {
+            // console.log("--------------");
+            // console.log(JSON.stringify(msg));
+            // console.log("--------------");
+            request.post(webhook_url, {
+                json: msg,
+            }, (error, res, body) => {
+                if (error) {
+                    console.error(error)
+                    return
+                }
+                console.log(`statusCode: ${res.statusCode}`)
+                console.log(body)
+                msg["blocks"] = []
+                msg_count = 0
+            })
+
+        }
+    } else {
+        if (!warned) {
+            console.log("WEBHOOK_URL not defined");
+            warned = true
+        }
+
     }
-    msg["blocks"].push({
+
+
+
+
+}
+
+function list_item(title, description, link) {
+    return {
+        "type": "mrkdwn",
+        // "text": `> [*${title+""}*](${link})`,
+        "text": `> <${link}|${title}> | ${description}`,
+    }
+}
+
+function block_item(title, link, description, extract, image) {
+    return {
         "type": "section",
         "text": {
             "type": "mrkdwn",
-            "text": `*${data.title+""}* || ${data.description} || ${data.extract}`
+            "text": `[*${title+""}*](${link}) || ${description} || ${extract}`
         },
         "accessory": {
             "type": "image",
-            "image_url": `${data.image?data.image:default_img}`,
-            "alt_text": `${data.title+""}`
+            "image_url": `${image?image:default_img}`,
+            "alt_text": `${title+""}`
         },
 
-    }, {
-        "type": "divider"
-    }, )
-
-
-
-    if (msg["blocks"].length > 10) {
-        // console.log("--------------");
-        // console.log(JSON.stringify(msg));
-        // console.log("--------------");
-
-        request.post(webhook_url, {
-            json: JSON.parse(JSON.stringify(msg)),
-        }, (error, res, body) => {
-            if (error) {
-                console.error(error)
-                return
-            }
-            console.log(`statusCode: ${res.statusCode}`)
-            console.log(body)
-            msg["blocks"] = []
-        })
     }
-
-
-
-}
-
-function get_webhook(url) {
-    request.get(url, {
-        
-    }, (error, res, body) => {
-        if (error) {
-            console.error(error)
-            return
-        }
-        webhook_url=body
-        // console.log(`statusCode: ${res.statusCode}`)
-        // console.log(body)
-        // msg["blocks"] = []
-    })
-    
 }
